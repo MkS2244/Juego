f5c739a71cc5def50b5593cbeae0057a09db44f2
MartaSM <13161523@alu.murciaeduca.es>
2024-11-12 15:03:48 +0000

    new file:   assets/img/frogger-logo.png 	new file:   assets/img/mapa1.png 	new file:   assets/img/mapa2.png 	new file:   assets/sonido/sound-frogger-hop.wav 	new file:   assets/sonido/sound-frogger-plunk.wav 	new file:   assets/sonido/sound-frogger-squash.wav

--- Frogger.js
+++ Frogger.js
@@ -1,3 +1,11 @@
 window.onload = function(){
+    //defino como constantes los topes del canvas
+    const TOPEDERECHA ;
+    const TOPEIZQUIERDA; 
+    const TOPEARRIBA;
+    const TOPEABAJO;
     
+    let canvas, ctx ; // variable para referenciar el canvas y el contexto de trabajo
+    let xDerecha, xIzquierda, yArriba, yAbajo, idAnimacion; //variables para la dirección del personaje
+    let posicion =0; //posición del array 
 }
