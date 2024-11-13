c34fc2c3724349f1dffd6c98c2d01d7567140efb
MartaSM <13161523@alu.murciaeduca.es>
2024-11-13 14:49:43 +0000

    modified:   Frogger.css

--- Frogger.js
+++ Frogger.js
@@ -4,11 +4,12 @@ window.onload = function () {
     const TOPEIZQUIERDA = 0;
     const TOPEARRIBA = 0;
     const TOPEABAJO = 575;
-
+    
     let canvas, ctx; // variable para referenciar el canvas y el contexto de trabajo
     let xDerecha, xIzquierda, yArriba, yAbajo, idAnimacion; //variables para la dirección del personaje
     let posicion = 0; //posición del array 
-
+    let x=0;    //posicion inicial x
+    let y=0;    //posicion inicial y
     let inicio = 0;
     let imagenRana;
     let miRana;
@@ -16,18 +17,48 @@ window.onload = function () {
     function Rana(x_, y_) {
         this.x = x_;
         this.y = y_;
-        this.animacionRana = [[1, 0], [75, 0], [75, 0], [150, 0], [0, 86], [76, 86], [76, 86], [150, 86], [182, 0], [77, 182], [93, 182], [170, 182], [0, 267], [77, 267], [92, 267], [167, 267]];
-        //abajo                            arriba                               izquierda                              derecha         
-        this.velocidad = 1.2;
-        this.tamañoX = 77;
-        this.tamañoY = 80;
+        this.animacionRana = [[1,1], [17,1],[19,1],[35, 1], [37, 1],[53, 1],[55, 1],[71, 1], [73,1],[89,1],[91,1],[107,1], [109,1],[125,1],[127,1],[143,1]];
+        //                            arriba                          izquierda                      abajo                         derecha   
+        this.velocidad = 1;
+        this.tamañoX = 16;
+        this.tamañoY = 16;
     }
 
-    Rana.prototype.generaPosicionDerecha = function () {
-        this.x = this.x + this.velocidad;
 
-        if (this.x > TOPEDERECHA) this.x = TOPEDERECHA;
+// --------------------------    GENERAR LAS POSICIONES ------------------------------
+Rana.prototype.generaPosicionDerecha = function () {
+    this.x = this.x + this.velocidad;
+
+    if (this.x > TOPEDERECHA) this.x = TOPEDERECHA;
+}
+
+Rana.prototype.generaPosicionIzquierda = function (){
+
+    this.x = this.x - this.velocidad;
+
+    if(this.x < TOPEIZQUIERDA){
+        this.x = TOPEIZQUIERDA;
     }
+}
+
+Rana.prototype.generaPosicionArriba = function () {
+    this.y = this.y - this.velocidad;
+
+    if (this.y < TOPEARRIBA){
+        this.y = TOPEARRIBA;
+    }
+}
+
+Rana.prototype.generaPosicionAbajo = function (){
+    this.y = this.y + this.velocidad;
+    if (this.y > TOPEABAJO){
+        this.y = TOPEABAJO;
+    }
+}
+
+
+
+// ------------ PINTAR EN EL CANVAS -------------
 
     function pintarRana() {
         //limpio el canvas
@@ -35,6 +66,7 @@ window.onload = function () {
 
         if (xDerecha) miRana.generaPosicionDerecha();
 
+        
         //pinto la Rana
         ctx.drawImage(
             miRana.imagenRana, //imagen completa 
@@ -47,10 +79,14 @@ window.onload = function () {
             miRana.tamañoX,
             miRana.tamañoY
         );
+
     }
 
     function movimientoRana() {
-        if (xDerecha) inicio = 0;
+        if (xDerecha) inicio = 12;
+        if (xIzquierda) inicio = 4;
+        if (yAbajo) inicio = 8;
+        if (yArriba) inicio = 0;
 
         posicion = inicio + (posicion + 1) % 2;
     }
@@ -103,16 +139,26 @@ window.onload = function () {
         }
 
     }
+//  Evento para saber cuando se presiona una tecla
+    document.addEventListener("keydown",activaMovimiento,false);
+    document.addEventListener("keyup", desactivaMovimiento, false);
 
-    //localizo el canvas y genero contexto de trabajo 
+//    localizo el canvas y genero contexto de trabajo 
     canvas = document.getElementById("miCanvas");
     ctx = canvas.getContext("2d");
 
     imagenRana = new Image();
-    imagenRana.src = "SpriteRanas.png";
+    imagenRana.src = "assets/img/ArcadeFrogger.png";
     Rana.prototype.imagenRana = imagenRana;
 
     //creo un objeto de Rana
     miRana = new Rana(x, y);
+
+//  Lanzo la animación
+    idAnimacion = setInterval(pintarRana, 1000/50);
+
+//  Animación encargada de hacer el movimiento de la Rana
+    let idMovimiento = setInterval(movimientoRana, 1000);
+
 
 }
