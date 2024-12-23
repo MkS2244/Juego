4a65805d724f2c3e9a115692640e52a824c26622
MartaSM <13161523@alu.murciaeduca.es>
2024-12-23 14:02:47 +0000

    colision rana-coche, gestion de puntuación

--- Frogger.js
+++ Frogger.js
@@ -2,6 +2,7 @@ window.onload = function () {
     // -------------------------------------------------
     //  VARIABLES Y CONSTANTES 
     // -------------------------------------------------
+    let nombre = prompt("Introduce tu nombre:");
 
     let xDerecha, xIzquierda, yArriba, yAbajo;      //variables de movimiento
 
@@ -22,7 +23,7 @@ window.onload = function () {
     let botonPausar;                                // Boton para pausar el juego
     let botonReanudar;                              // Boton para reanudar el juego
 
-    //let puntuacion;                                 // Puntuacion del juego
+    let puntuacion;                                 // Puntuacion del juego
 
     function reproducirAudio(audio_) {
         audio_.currentTime = 0;
@@ -31,6 +32,40 @@ window.onload = function () {
 
     function pintarFondo(ctx_) {
         ctx_.drawImage(Fondo, 0, 0, 600, 400);
+    }
+
+
+    function guardarPartida(nombre, puntuacion) {
+        puntuacion = obtenerTiempo();
+        //obtengo el tiempo de la partida 
+
+        // Para recuperar partidas que ya existen o inicializar un array vacío si no existen
+        let partida = JSON.parse(localStorage.getItem("partida")) || [];
+        // Agregar la partida nueva con el nombre y la puntuación
+        partida.push({ nombre, puntuacion });
+        records.sort((a, b) => b.puntuacion - a.puntuacion);
+        // Guardar en LocalStorage
+        localStorage.setItem("partida", JSON.stringify(partida));
+    }
+
+    function mostrarPartida(ctxInfo_) {
+        let partida = JSON.parse(localStorage.getItem("partida")) || [];
+        //limpio el canvas
+        ctxInfo_.clearRect(0, 0, 600, 100);
+        ctxInfo_.fillStyle = "white";
+        ctxInfo_.font = "20px Tiny5";
+
+        ctxInfo_.fillText("Historial de partidas", 10, 20);
+
+        // 
+        partida.slice(0, 5).forEach((partida, index) => {
+            ctxInfo_.fillText(`${index + 1}. ${partida.nombre} - ${partida.puntuacion}`, 10, 40 + (index * 20));
+        });
+    }
+
+    function resetRecord(){
+        localStorage.removeItem("partida");
+        console.log("Partidas borradas");
     }
 
     // -------------------------------------------------
@@ -82,40 +117,40 @@ window.onload = function () {
         // console.table("Tortugas", arrayTortugas)
     }
 
-    function generaTroncos(){
-        let yPosiciones = [39,70, 130];
+    function generaTroncos() {
+        let yPosiciones = [39, 70, 130];
         // Posiciones en las que salen los troncos en el mapa
         let delay = 1000;
 
-        if (contadorTroncos < 6){
-        
-            for (let i = 0; i <3; i++){
-                let yPosicion = yPosiciones[i];        
+        if (contadorTroncos < 6) {
+
+            for (let i = 0; i < 3; i++) {
+                let yPosicion = yPosiciones[i];
                 setTimeout(() => {
                     tronco = new Tronco(yPosicion);
                     arrayTroncos.push(tronco);
                 }, i * delay);
             }
-            contadorTroncos +=3;
+            contadorTroncos += 3;
         }
 
         // console.table("TRONCOS ", arrayTroncos);
 
-        if (contadorTroncos === 6 ){
+        if (contadorTroncos === 6) {
             contadorTroncos = 0;
         }
     }
 
-    function generaCamiones (){
+    function generaCamiones() {
         let yPosiciones = [215, 285];
         // Posiciones en las que salen los camiones en el mapa
         let delay = 8000;
 
-        if (contadorCamion < 4){
-            let yPosicion = (contadorCamion < 2) ? yPosiciones[0]: yPosiciones[1];
+        if (contadorCamion < 4) {
+            let yPosicion = (contadorCamion < 2) ? yPosiciones[0] : yPosiciones[1];
 
-            for (let i = 0 ; i < 2 ; i++){
-            
+            for (let i = 0; i < 2; i++) {
+
                 setTimeout(() => {
                     camion = new Camion(yPosicion);
                     arrayCamiones.push(camion);
@@ -124,62 +159,65 @@ window.onload = function () {
             contadorCamion += 2;
         }
 
-        if (contadorCamion === 4){
+        if (contadorCamion === 4) {
             contadorCamion = 0;
         }
     }
 
-    function generaCoches (){
+    function generaCoches() {
         let yPosiciones = [245, 320];
         // Posiciones en las que salen los coches en el mapa
         let delayCoche1 = 5500;
         let delayCoche2 = 3500;
         let delayCoche3 = 1500;
 
-        if (contadorCoches < 3){
-            let yPosicion = (contadorCoches < 1) ? yPosiciones[0]: yPosiciones[1];
+        if (contadorCoches < 3) {
+            let yPosicion = (contadorCoches < 1) ? yPosiciones[0] : yPosiciones[1];
 
+            // genero el coche 1
             setTimeout(() => {
-                coche1 = new Coche (yPosicion, "coche1");
+                coche1 = new Coche(yPosicion, "coche1");
                 arrayCoches.push(coche1);
             }, delayCoche1);
-            
+
+            // genero el coche 2
             setTimeout(() => {
-                coche2 = new Coche (yPosicion, "coche2");
+                coche2 = new Coche(yPosicion, "coche2");
                 arrayCoches.push(coche2);
             }, delayCoche2);
 
+            // genero el coche 3
             setTimeout(() => {
-                coche3 = new Coche (yPosicion, "coche3");
+                coche3 = new Coche(yPosicion, "coche3");
                 arrayCoches.push(coche3);
             }, delayCoche3);
 
-/*             // genero el coche 1
-            for (let i = 0 ; i < 1 ; i++){
-                setTimeout(() => {
-                    coche1 = new Coche (yPosicion, "coche1");
-                    arrayCoches.push(coche1);
-                }, i * delayCoche1);
-            }
-            // genero el coche 2
-            for (let i = 0 ; i < 1 ; i++){
-                setTimeout(() => {
-                    coche2 = new Coche (yPosicion, "coche2");
-                    arrayCoches.push(coche2);
-                }, i * delayCoche2);
-            }
-            // genero el coche 3
-            for (let i = 0 ; i < 1 ; i++){
-                setTimeout(() => {
-                    coche3 = new Coche (yPosicion, "coche3");
-                    arrayCoches.push(coche3);
-                }, i * delayCoche3);
-            } */
+            /*             
+                        for (let i = 0 ; i < 1 ; i++){
+                            setTimeout(() => {
+                                coche1 = new Coche (yPosicion, "coche1");
+                                arrayCoches.push(coche1);
+                            }, i * delayCoche1);
+                        }
+                        // genero el coche 2
+                        for (let i = 0 ; i < 1 ; i++){
+                            setTimeout(() => {
+                                coche2 = new Coche (yPosicion, "coche2");
+                                arrayCoches.push(coche2);
+                            }, i * delayCoche2);
+                        }
+                        // genero el coche 3
+                        for (let i = 0 ; i < 1 ; i++){
+                            setTimeout(() => {
+                                coche3 = new Coche (yPosicion, "coche3");
+                                arrayCoches.push(coche3);
+                            }, i * delayCoche3);
+                        } */
 
             contadorCoches++;
         }
 
-        if (contadorCoches === 3){
+        if (contadorCoches === 3) {
             contadorCoches = 0;
         }
     }
@@ -189,22 +227,22 @@ window.onload = function () {
             arrayTortugas[i].pintarTortuga(ctx);
             arrayTortugas[i].moverTortuga();
 
-            if (arrayTortugas[i].desapareceDelMapa()) arrayTortugas[i].acabado = true; 
+            if (arrayTortugas[i].desapareceDelMapa()) arrayTortugas[i].acabado = true;
 
         }
     }
 
-    function muevoTroncos (){
-        for (let i = 0; i < arrayTroncos.length; i++){
+    function muevoTroncos() {
+        for (let i = 0; i < arrayTroncos.length; i++) {
             arrayTroncos[i].pintarTronco(ctx);
             arrayTroncos[i].moverTronco();
 
-            if (arrayTroncos[i].desapareceDelMapa()) arrayTroncos[i].acabado = true; 
+            if (arrayTroncos[i].desapareceDelMapa()) arrayTroncos[i].acabado = true;
         }
     }
 
-    function muevoCamiones (){
-        for (let i = 0; i < arrayCamiones.length; i++){
+    function muevoCamiones() {
+        for (let i = 0; i < arrayCamiones.length; i++) {
             arrayCamiones[i].pintarCamion(ctx);
             arrayCamiones[i].moverCamion();
 
@@ -212,11 +250,12 @@ window.onload = function () {
         }
     }
 
-    function muevoCoches (){
-        for (let i = 0; i < arrayCoches.length; i++){
+    function muevoCoches() {
+        for (let i = 0; i < arrayCoches.length; i++) {
             arrayCoches[i].pintarCoches(ctx);
             arrayCoches[i].moverCoches();
 
+            //console.table(arrayCoches[i]);
             if (arrayCoches[i].desapareceDelMapa()) arrayCoches[i].acabado = true;
         }
     }
@@ -251,45 +290,27 @@ window.onload = function () {
     }
 
     // -------------------------------------------------
-    //  FUNCION PARA GENERAR LA ANIMACION DEL JUEGO
+    //  COLISIONES DEL JUEGO
     // -------------------------------------------------
-
-    function generaAnimacionRana() {
-
-        //limpio el canvas
-        ctx.clearRect(0, 0, 600, 400);
-
-        //pinto el mapa del juego
-        pintarFondo(ctx);
-
-        //Muevo los objetos del juego (troncos, tortugas y coches)
-        rana.pintar(ctx);
-
-        if(ranaMuerta()){
-            clearInterval(idRana);
-            botonNuevaPartida.disabled = true;
-            botonPausar.disabled = true;
-        }
-    }
 
     function ranaMuerta() {
         let i = 0;
-    
+
         let rIzq = rana.x;
         let rDer = rana.x + rana.tamañoX;
         let rDown = rana.y;
         let rUp = rana.y + rana.tamañoY;
-    
+
         do {
-            if (i >= arrayCoches.length){
+            if (i >= arrayCoches.length) {
                 break;
             }
-    
+
             let oIzq = Math.round(arrayCoches[i].x);
-            let oDer = Math.round(arrayCoches[i].x + arrayCoches[i].ancho);
+            let oDer = Math.round(arrayCoches[i].x + arrayCoches[i].tamañoX - 20);
             let oDown = Math.round(arrayCoches[i].y);
-            let oUp = Math.round(arrayCoches[i].y + nObstaculos[i].alto);
-    
+            let oUp = Math.round(arrayCoches[i].y + arrayCoches[i].tamañoY - 20);
+
             if ((rDer > oIzq) &&
                 (rIzq < oDer) &&
                 (rUp > oDown) &&
@@ -299,8 +320,35 @@ window.onload = function () {
                 i++;
             }
         } while (!estamosMuertos);
-    
+
         return estamosMuertos;
+    }
+
+    // -------------------------------------------------
+    //  FUNCION PARA GENERAR LA ANIMACION DEL JUEGO
+    // -------------------------------------------------
+
+    function generaAnimacionRana() {
+
+        //limpio el canvas
+        ctx.clearRect(0, 0, 600, 400);
+        ctxInfo.clearRect(0, 0, 600, 100);
+        //pinto el mapa del juego
+        pintarFondo(ctx);
+
+        //Muevo los objetos del juego (troncos, tortugas y coches)
+        rana.pintar(ctx);
+
+        if (ranaMuerta()) {
+            console.log("Partida terminada");
+            detenerJuego();
+            textoGameOver();
+            
+            
+            botonNuevaPartida.disabled = false;
+            botonPausar.disabled = true;
+
+        }
     }
 
     // -------------------------------------------------
@@ -353,8 +401,8 @@ window.onload = function () {
         if (juegoPausado) {
             clearInterval(idRana);
             ctxInfo.fillStyle = "white";
-            ctxInfo.font = "20px Arial";
-            ctxInfo.fillText("Juego Pausado", 150 / 2 - 70, 400 / 2);
+            ctxInfo.font = "20px Tiny5";
+            ctxInfo.fillText("Juego Pausado", 600 / 2 - 100, 100 / 2);
 
         }
 
@@ -364,19 +412,19 @@ window.onload = function () {
     //  FUNCION PARA REANUDAR EL JUEGO
     // -------------------------------------------------
 
-/*     function reanudarPartida() {
-
-        if (juegoPausado) {
-            juegoPausado = false;
-            idRana = setInterval(animacionRana, 50 / 1000);
-            idAnimacion = setInterval(generaAnimacion, 50 / 1000); // Reanuda la animación del juego
-            botonPausar.disabled = false;
-            botonReanudar.disabled = true;
-            botonNuevaPartida.disabled = false;
-
+    /*     function reanudarPartida() {
+    
+            if (juegoPausado) {
+                juegoPausado = false;
+                idRana = setInterval(animacionRana, 50 / 1000);
+                idAnimacion = setInterval(generaAnimacion, 50 / 1000); // Reanuda la animación del juego
+                botonPausar.disabled = false;
+                botonReanudar.disabled = true;
+                botonNuevaPartida.disabled = false;
+    
+            }
         }
-    }
- */
+     */
 
     // -------------------------------------------------
     //  FUNCION PARA COMENZAR EL JUEGO
@@ -388,6 +436,8 @@ window.onload = function () {
         /* botonReanudar.disabled = false; */
 
         inicioDeVariables();
+        // Para la gestion de la puntuación que será por tiempo
+        let tiempoInicio = Date.now();
 
         idAnimacion = setInterval(generaAnimacionRana, 1000 / 20);
         idRana = setInterval(animacionRana, 1000 / 20);
@@ -410,6 +460,41 @@ window.onload = function () {
         document.addEventListener("keyup", desactivaMovimiento, false);
     }
 
+    function textoGameOver() {
+        ctxInfo.fillStyle = "#33cc00";
+        ctxInfo.font = "40px Tiny5";
+        ctxInfo.fillText("GAME  OVER", 600 / 2 - 100, 100 / 2);
+    }
+
+    function detenerJuego() {
+        textoGameOver();
+        //para que se muestre primero el GAME OVER y despues la puntuacion
+        setTimeout(() => {
+            guardarPartida(nombre, puntuacion);
+            mostrarPartida(ctxInfo);
+
+            clearInterval(idRana);
+            clearInterval(idAnimacion);
+            clearInterval(idGeneraTortugas);
+            clearInterval(idTortuga);
+            clearInterval(idGeneraTroncos);
+            clearInterval(idTronco);
+            clearInterval(idGeneraCamiones);
+            clearInterval(idCamion);
+            clearInterval(idGeneraCoches);
+            clearInterval(idCoches);
+        }, 2000);
+    }
+    // ------------------------
+    //  PUNTUACIÓN
+    // ------------------------
+
+    function obtenerTiempo(){
+        let tiempoFinal = Date.now();
+        let tiempoTotal = (tiempoFinal - tiempoInicio) / 1000; // tiempo en segundos
+
+        return tiempoTotal.toFixed(2); //redondeo a dos decimales
+    }
     // ------------------------
     //  BOTONES
     // ------------------------
@@ -419,6 +504,10 @@ window.onload = function () {
 
     botonPausar = document.getElementById("pausaPartida");
     botonPausar.onclick = pausarPartida;
+
+    botonReset = document.getElementById("reset").addEventListener("click", function(){
+        resetRecord();
+    });
 
     /*     botonReanudar = document.getElementById("reanudarPartida");
         botonReanudar.onclick = reanudarPartida;
